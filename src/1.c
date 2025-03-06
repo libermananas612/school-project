1d6539bed478fe958d7155c48925b4f5d74aabe7
libermananas612 <libermananas612@gmail.com>
2025-03-06 06:52:58 +0000

    Add 1.c

---
+++ src/1.c
@@ -0,0 +1,10 @@
+// Example of how to add two numbers using C programming language
+#include <stdio.h>
+
+int main() {
+   int num1 = 5;
+   int num2 = 10;
+   int sum = num1 + num2;
+   printf("The sum is %d", sum);
+   return 0;
+}
