39dab7fde1193f967351e82d82d4b92c58ea4af2
libermananas612 <libermananas612@gmail.com>
2025-03-19 23:36:48 +0000

    Add 13.c

---
+++ src/13.c
@@ -0,0 +1,9 @@
+int main() {
+  int age = 21;
+  if (age >= 18) {
+    printf("You are old enough to vote!\n");
+  } else {
+    printf("Sorry, you are not yet old enough to vote.\n");
+  }
+  return 0;
+}
