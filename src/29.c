0edd7ce18f998c255bb670a52905af7a77d6cd73
libermananas612 <libermananas612@gmail.com>
2025-04-07 05:35:04 +0000

    Add 29.c

---
+++ src/29.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int i;
+    printf("Hello, World!\n");
+    for (i = 0; i < 5; i++) {
+        if (i % 2 == 0)
+            continue;
+        else
+            printf("%d", i);
+    }
+}
